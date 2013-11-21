abf799688a78804a73f9c4dd56bcb9b083624b11
Ferris Tseng <ferristseng@gmail.com>
2013-11-21 15:37:42 -0500

    moved parse functions into parse.rs, and changed how ps was handled in function

--- parse.rs
+++ parse.rs
@@ -1,3 +1,5 @@
+use std::ptr;
+
 // A Regex Operation
 pub enum OpCode {
   OpConcatenation,
@@ -15,6 +17,8 @@ pub enum OpCode {
 // Flags for parsing 
 // i'm including a bunch of extra flags for now,
 // but we can add support for them as we go
+//
+// actually, not really sure how these work
 enum ParseFlags {
   NoParseFlags  = 0,
   FoldCase      = 1 << 0
@@ -342,3 +346,176 @@ impl ParseState {
     }
   }
 }
+
+// parse functions
+//
+// these take in a pointer to a ParseState and an input string,
+// and finish / modify the ParseState
+
+pub fn parse_charclass(t: &mut ~str, s: *mut ParseState) -> Result<ParseState, &'static str> {
+ 
+  let mut ps = unsafe { ptr::read_and_zero_ptr(s) };
+
+  // check to see if the first char following
+  // '[' is a '^', if so, it is a negated char 
+  // class
+  let mut cc = CharClass::new();
+
+  // we need to keep track of any [, ( in
+  // the input, because we can just ignore 
+  // them
+  let mut nbracket: uint = 0;
+
+  match t.char_at(0) {
+    '^' => {
+      t.shift_char();
+      cc.negate();
+    },
+    _ => { }
+  };
+
+  while (t.len() > 0) {
+
+    match t.char_at(0) {
+      '[' => {
+        nbracket += 1;
+        t.shift_char();
+      },
+      ']' => {
+        t.shift_char();
+        if (nbracket > 0) {
+          nbracket -= 1;
+        } else {
+          ps.pushCharClass(cc);
+          return Ok(ps);
+        }
+      }
+      c => {
+        t.shift_char();
+        
+        // check to see if its this is part of a 
+        // range
+        if (t.len() > 1) {
+          match t.char_at(0) {
+            '-' => {
+              if (t.char_at(1) != ']') {
+                match cc.addRange(c, t.char_at(1)) {
+                  Err(e) => return Err(e),
+                  _ => { } // Ok...continue
+                }
+                t.shift_char();
+                t.shift_char();
+              }
+            }
+            _ => { 
+              cc.addChar(c);
+            }
+          }
+        } else {
+          cc.addChar(c); 
+        }
+      }
+    }
+
+  }
+
+  Err("Expected a ']'.")
+}
+
+// parse an input string recursively
+// ideally, we wouldn't parse an input string recursively
+// because rust does not optimize tail end
+// recursive calls, but...
+// this way is pretty
+pub fn parse_recursive(t: &mut ~str, s: *mut ParseState) -> Result<ParseState, &'static str> {
+  
+  let mut ps = unsafe { ptr::read_and_zero_ptr(s) };
+
+  // check for an err,
+  // if not update the state
+  macro_rules! set_ok(
+    ($f: expr) => (
+      match $f {
+        Ok(s)   => { ps = s; }
+        Err(e)  => return Err(e)
+      }
+    );
+  )
+
+  // cases for
+  // parsing different characters
+  // in the input string
+  while (t.len() > 0) {
+
+    match t.char_at(0) {
+      '(' => {
+        ps.doConcatenation();
+        ps.pushLeftParen();
+
+        t.shift_char();
+        set_ok!(parse_recursive(t, ptr::to_mut_unsafe_ptr(&mut ps)));
+        ps.doLeftParen();
+      },
+      ')' => {
+        t.shift_char();
+        if (ps.hasUnmatchedParens()) {
+          break;
+        }
+        return Err("Unmatched ')'")
+      }
+
+      '|' => {
+        ps.doConcatenation();
+        ps.pushAlternation();
+
+        t.shift_char();
+        set_ok!(parse_recursive(t, ptr::to_mut_unsafe_ptr(&mut ps)));
+        ps.doAlternation();
+        
+        if (ps.hasUnmatchedParens()) {
+          break;
+        }
+      },
+
+      '*' => {
+        t.shift_char();
+        ps.doKleine();
+      },
+      '?' => {
+        t.shift_char();
+        ps.doZeroOrOne();
+      },
+      '+' => {
+        t.shift_char();
+        ps.doOneOrMore();
+      }
+
+      '[' => {
+        t.shift_char();
+        set_ok!(parse_charclass(t, ptr::to_mut_unsafe_ptr(&mut ps)));
+      }
+
+      c => {
+        ps.pushLiteral(c.to_str());
+        t.shift_char();
+      }
+    }
+
+    ps.trace();
+
+  }
+
+  ps.doConcatenation();
+
+  // replace the content at
+  // the old pointer, if a state was passed in
+  /*match s {
+    Some(st) => {
+      unsafe { ptr::replace_ptr(st, ps); }
+    },
+    _ => { }
+  }*/
+
+  Ok(ps)
+}
+
--- regexp.rs
+++ regexp.rs
@@ -8,170 +8,17 @@ struct Regexp {
 }
 
 impl Regexp {
-  fn new(s: &str) -> Regexp {
+  pub fn new(s: &str) -> Regexp {
     Regexp { input: s.clone().to_owned() }
   }
+}
+
+impl Regexp { 
+  // we should hide the underlying parsing algorithm
   fn parse(&mut self) {
     let mut ps = ParseState::new();
-    parse_recursive(&mut self.input, Some(ptr::to_mut_unsafe_ptr(&mut ps))); 
+    parse_recursive(&mut self.input, ptr::to_mut_unsafe_ptr(&mut ps)); 
   }
-}
-
-pub fn parse_charclass(t: &mut ~str, s: *mut ParseState) -> Result<(), &'static str> {
- 
-  let mut ps = unsafe { ptr::read_and_zero_ptr(s) };
-
-  // check to see if the first char following
-  // '[' is a '^', if so, it is a negated char 
-  // class
-  let mut cc = CharClass::new();
-
-  // we need to keep track of any [, ( in
-  // the input, because we can just ignore 
-  // them
-  let mut nbracket: uint = 0;
-
-  match t.char_at(0) {
-    '^' => {
-      t.shift_char();
-      cc.negate();
-    },
-    _ => { }
-  };
-
-  while (t.len() > 0) {
-
-    match t.char_at(0) {
-      '[' => {
-        nbracket += 1;
-        t.shift_char();
-      },
-      ']' => {
-        t.shift_char();
-        if (nbracket > 0) {
-          nbracket -= 1;
-        } else {
-          ps.pushCharClass(cc);
-          break;
-        }
-      }
-      c => {
-        t.shift_char();
-        
-        // check to see if its this is part of a 
-        // range
-        if (t.len() > 1) {
-          match t.char_at(0) {
-            '-' => {
-              if (t.char_at(1) != ']') {
-                match cc.addRange(c, t.char_at(1)) {
-                  Err(e) => return Err(e),
-                  _ => { } // Ok...continue
-                }
-                t.shift_char();
-                t.shift_char();
-              }
-            }
-            _ => { 
-              cc.addChar(c);
-            }
-          }
-        } else {
-          cc.addChar(c); 
-        }
-      }
-    }
-
-  }
-
-  unsafe { ptr::replace_ptr(s, ps); }
-
-  Err("Expected a ']'.")
-}
-
-pub fn parse_recursive(t: &mut ~str, s: Option<*mut ParseState>) -> Result<(), &'static str> {
-  let mut ps = match s {
-    Some(st) => { 
-      unsafe { ptr::read_and_zero_ptr(st) } 
-    },
-    None => ParseState::new()
-  };
-
-  // cases for
-  // parsing different characters
-  // in the input string
-  while (t.len() > 0) {
-
-    match t.char_at(0) {
-      '(' => {
-        ps.doConcatenation();
-        ps.pushLeftParen();
-
-        t.shift_char();
-        parse_recursive(t, Some(ptr::to_mut_unsafe_ptr(&mut ps)));
-        ps.doLeftParen();
-      },
-      ')' => {
-        t.shift_char();
-        if (ps.hasUnmatchedParens()) {
-          break;
-        }
-        return Err("Unmatched ')'")
-      }
-
-      '|' => {
-        ps.doConcatenation();
-        ps.pushAlternation();
-
-        t.shift_char();
-        parse_recursive(t, Some(ptr::to_mut_unsafe_ptr(&mut ps)));
-        ps.doAlternation();
-        
-        if (ps.hasUnmatchedParens()) {
-          break;
-        }
-      },
-
-      '*' => {
-        t.shift_char();
-        ps.doKleine();
-      },
-      '?' => {
-        t.shift_char();
-        ps.doZeroOrOne();
-      },
-      '+' => {
-        t.shift_char();
-        ps.doOneOrMore();
-      }
-
-      '[' => {
-        t.shift_char();
-        parse_charclass(t, ptr::to_mut_unsafe_ptr(&mut ps));
-      }
-
-      c => {
-        ps.pushLiteral(c.to_str());
-        t.shift_char();
-      }
-    }
-
-    ps.trace();
-
-  }
-
-  ps.doConcatenation();
-
-  // replace the content at
-  // the old pointer, if a state was passed in
-  match s {
-    Some(st) => {
-      unsafe { ptr::replace_ptr(st, ps); }
-    },
-    _ => { }
-  }
-
-  Ok(())
 }
 
 fn main() {
