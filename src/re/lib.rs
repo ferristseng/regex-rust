9664ebb9087b6efc70da0f656614d8335a48ef23
Ferris Tseng <ferristseng@gmail.com>
2013-12-06 22:44:57 -0500

    added lib.rs back

---
+++ src/re/lib.rs
@@ -0,0 +1,21 @@
+#[link(
+  name = "re",
+  vers = "0.1",
+  url = "https://github.com/ferristseng/regex-rust/tree/master"
+ )];
+
+#[license = "MIT"];
+#[comment = "Regular Expression Engine in Rust"];
+#[crate_type = "lib"];
+
+extern mod extra;
+
+pub use regexp::UncompiledRegexp;
+
+mod error;
+mod exec;
+mod compile;
+mod parse;
+mod state;
+
+pub mod regexp;
