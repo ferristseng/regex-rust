726b57257d4b18fbc44ef37c25cd786525105fb1
Brian Whitlow <clgy15@gmail.com>
2014-04-09 22:17:55 -0400

    Benchmarking basic completed
    The benchmarking system is instantiated.
    Rust Language added.
    Parse/Execute Loop Benchmark added.

---
+++ benchmark/generators/cases.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Add cases below
+# These are used to generated a file of benchmarks used across multiple testing languages
+#
+
+NO_LOOPS = 50
+
+# These are the tests we generate functions for
+# (re, input)
+TESTS = [
+  ("^abc", "abcc"),
+  # ("\\P{Greek}", "\u0374", "\u0374", MATCH)
+  ("^abc", "hslc")
+]
