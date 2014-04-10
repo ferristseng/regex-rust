59ec1e243e8b68c9681a680208dac4feb8235c85
Brian Whitlow <clgy15@gmail.com>
2014-04-09 22:45:23 -0400

    C++11 Added

--- benchmark/generators/cases.py
+++ benchmark/generators/cases.py
@@ -4,7 +4,7 @@
 # These are used to generated a file of benchmarks used across multiple testing languages
 #
 
-NO_LOOPS = 50
+NO_LOOPS = 10000
 
 # These are the tests we generate functions for
 # (re, input)
